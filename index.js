371af543d28a109f011dcea2e702497a9f46f94f
Kevin Karma <kevinkarma55@gmail.com>
2019-10-17 20:41:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,86 @@
-/* Your Code Here */
+function createEmployeeRecord(arr) {
+  return {
+    firstName : arr[0],
+    familyName : arr[1],
+    title : arr[2],
+    payPerHour : arr[3],
+    timeInEvents : [],
+    timeOutEvents : []
+  }
+}
+
+function createEmployeeRecords(arrOfArr) {
+  let employeeArray = [];
+  arrOfArr.forEach((employee)=>{
+    employeeArray.push(createEmployeeRecord(employee));
+  })
+  return employeeArray;
+}
+
+function createTimeInEvent(dateStamp) {
+  const fullDate = dateStamp.split(" ");
+  const date =  fullDate[0];
+  const time = fullDate[1];
+  const info = {
+    type : "TimeIn",
+    hour : parseInt(time),
+    date : date
+  };
+  this.timeInEvents.push(info)
+  return this
+}
+
+function createTimeOutEvent(dateStamp){
+  const fullDate = dateStamp.split(" ");
+  const date =  fullDate[0];
+  const time = fullDate[1];
+  const info = {
+    type : "TimeOut",
+    hour : parseInt(time),
+    date : date
+  };
+  this.timeOutEvents.push(info);
+  return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+  let hoursWorked = 0;
+  const dayIn =this.timeInEvents.find((date) => {return date.date === dateStamp});
+  const dayOut = this.timeOutEvents.find((date) =>{return date.date === dateStamp});
+  hoursWorked = (dayOut.hour - dayIn.hour)/100;
+  return hoursWorked;
+}
+
+function wagesEarnedOnDate(dateStamp) {
+  return hoursWorkedOnDate.call(this,dateStamp) * this.payPerHour;
+}
+
+// function allWagesFor(record) {
+//   let totalWages = 0;
+//   record.timeInEvents.forEach((timeIn)=>{
+//       totalWages += wagesEarnedOnDate(record,timeIn.date);
+//       //console.log("Total Wages: ===================================="+totalWages)
+//   })
+//   return totalWages;
+// }
+
+function findEmployeeByFirstName(arr,firstName) {
+  return arr.find((employee)=>{
+    return employee.firstName === firstName;
+  })
+}
+
+function calculatePayroll(arr) {
+  let totalWages = 0;
+  arr.forEach((employee)=>{
+    totalWages += allWagesFor.call(employee);
+  })
+  return totalWages;
+}
+
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
